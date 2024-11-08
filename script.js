507d1c200209b0a8fb5ca7b05b01bd5058dad473
gabangsr <gabangsram@gmail.com>
2024-11-08 22:02:35 +0800

    Replaced Search bar with randomizer, added new cards for armor pieces

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-function fetchSwitchAxes() {
+function fetchSwitchAxesAndArmor() {
     fetch('https://mhw-db.com/weapons')
         .then(response => response.json())
         .then(data => {
-            const randomWeapon1 = data[Math.floor(Math.random() * data.length)];
-            const randomWeapon2 = data[Math.floor(Math.random() * data.length)];
+            const switchAxes = data.filter(weapon => weapon.type === "switch-axe" && (weapon.rarity === 7 || weapon.rarity === 8));
+            const randomWeapon1 = switchAxes[Math.floor(Math.random() * switchAxes.length)];
             const weaponImage1 = randomWeapon1.assets ? randomWeapon1.assets.image : '';
-            const weaponImage2 = randomWeapon2.assets ? randomWeapon2.assets.image : '';
             const weaponCard1 = document.getElementById('weaponCard1');
             const weaponImage1Element = document.getElementById('weaponImage1');
             weaponImage1Element.src = weaponImage1;
             weaponCard1.querySelector('h2').innerText = randomWeapon1.name;
-            const weaponCard2 = document.getElementById('weaponCard2');
-            const weaponImage2Element = document.getElementById('weaponImage2');
-            weaponImage2Element.src = weaponImage2;
-            weaponCard2.querySelector('h2').innerText = randomWeapon2.name;
         })
         .catch(error => {
             console.error('Error fetching weapon data:', error);
         });
+
+
+    fetch('https://mhw-db.com/armor')
+        .then(response => response.json())
+        .then(data => {
+            const gender = "imageMale";
+            const armorTypes = ['head', 'chest', 'gloves', 'waist', 'legs'];
+            armorTypes.forEach((type, index) => {
+                const filteredArmor = data.filter(armor => armor.type === type && (skillname => Partbreaker ));
+                const randomArmor = filteredArmor[Math.floor(Math.random() * filteredArmor.length)];
+                const armorImage = randomArmor.assets ? randomArmor.assets[gender] : '';
+                const armorCard = document.getElementById(`armorCard${index + 1}`);
+                const armorImageElement = document.getElementById(`armorImage${index + 1}`);
+                armorImageElement.src = armorImage;
+                armorCard.querySelector('h2').innerText = randomArmor.name;
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching armor data:', error);
+        });
 }
 
-fetchSwitchAxes();
+fetchSwitchAxesAndArmor();

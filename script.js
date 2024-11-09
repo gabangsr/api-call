24bd5bd96f7d7dc4c96fcc205ba31ffd1b190241
gabangsr <gabangsram@gmail.com>
2024-11-09 22:52:05 +0800

    Added Randomizer For Monster to hunt, no images currently present

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-function fetchSwitchAxesAndArmor() {
+function BraverySwitchAxe() {
     fetch('https://mhw-db.com/weapons')
         .then(response => response.json())
         .then(data => {
             const switchAxes = data.filter(weapon => weapon.type === "switch-axe" && (weapon.rarity === 7 || weapon.rarity === 8));
-            const randomWeapon1 = switchAxes[Math.floor(Math.random() * switchAxes.length)];
-            const weaponImage1 = randomWeapon1.assets ? randomWeapon1.assets.image : '';
-            const weaponCard1 = document.getElementById('weaponCard1');
-            const weaponImage1Element = document.getElementById('weaponImage1');
-            weaponImage1Element.src = weaponImage1;
-            weaponCard1.querySelector('h2').innerText = randomWeapon1.name;
+            const randomWeapon = switchAxes[Math.floor(Math.random() * switchAxes.length)];
+            const weaponCard = document.getElementById('weaponCard1');
+            const weaponImage = document.getElementById('weaponImage1');
+            weaponImage.src = randomWeapon.assets ? randomWeapon.assets.image : 'default-weapon.png';
+            weaponCard.querySelector('h2').innerText = randomWeapon.name;
         })
-        .catch(error => {
-            console.error('Error fetching weapon data:', error);
-        });
-
+        .catch(error => console.error('Error fetching weapon data:', error));
 
     fetch('https://mhw-db.com/armor')
         .then(response => response.json())
@@ -21,18 +17,26 @@ function fetchSwitchAxesAndArmor() {
             const gender = "imageMale";
             const armorTypes = ['head', 'chest', 'gloves', 'waist', 'legs'];
             armorTypes.forEach((type, index) => {
-                const filteredArmor = data.filter(armor => armor.type === type && (skillname => Partbreaker ));
+                const filteredArmor = data.filter(armor => armor.type === type && armor.assets && armor.assets[gender]);
                 const randomArmor = filteredArmor[Math.floor(Math.random() * filteredArmor.length)];
-                const armorImage = randomArmor.assets ? randomArmor.assets[gender] : '';
                 const armorCard = document.getElementById(`armorCard${index + 1}`);
                 const armorImageElement = document.getElementById(`armorImage${index + 1}`);
-                armorImageElement.src = armorImage;
+                armorImageElement.src = randomArmor.assets[gender];
                 armorCard.querySelector('h2').innerText = randomArmor.name;
             });
         })
-        .catch(error => {
-            console.error('Error fetching armor data:', error);
-        });
+        .catch(error => console.error('Error fetching armor data:', error));
+
+    fetch('https://mhw-db.com/monsters')
+        .then(response => response.json())
+        .then(data => {
+            const largeMonsters = data.filter(monster => monster.type === 'large');
+            const randomMonster = largeMonsters[Math.floor(Math.random() * largeMonsters.length)];
+            const monsterCard = document.getElementById('monsterCard');
+            monsterCard.querySelector('h2').innerText = randomMonster.name;
+            monsterCard.querySelector('p').innerText = randomMonster.description || "A terrifying force of nature that lurks in the shadows, awaiting its next victim...";
+        })
+        .catch(error => console.error('Error fetching monster data:', error));
 }
 
-fetchSwitchAxesAndArmor();
+BraverySwitchAxe();
